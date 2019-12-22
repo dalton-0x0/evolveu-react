a7b07530a558848135889b6339ba68a02905d78d
Dalton Okechukwu <chike911@yahoo.com>
2019-12-21 17:12:33 -0700

    Updates

---
+++ precache-manifest.1b2dad0d458f1ca164c0896a11317f0d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb4fd26bf32fc820d8e245273383e9d",
+    "url": "/evolveu-react/index.html"
+  },
+  {
+    "revision": "f9ae089308efe271411a",
+    "url": "/evolveu-react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "30b5038a78de7e571156",
+    "url": "/evolveu-react/static/css/main.c6f940a5.chunk.css"
+  },
+  {
+    "revision": "f9ae089308efe271411a",
+    "url": "/evolveu-react/static/js/2.d7729859.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/evolveu-react/static/js/2.d7729859.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30b5038a78de7e571156",
+    "url": "/evolveu-react/static/js/main.7ecb86c2.chunk.js"
+  },
+  {
+    "revision": "f585427aa43e472883d9",
+    "url": "/evolveu-react/static/js/runtime-main.b3052df4.js"
+  },
+  {
+    "revision": "b0be534a23a5c9db740617d0b0c3bd41",
+    "url": "/evolveu-react/static/media/001-home.b0be534a.svg"
+  },
+  {
+    "revision": "4c2748a2d92e2683b418d7cf19444dae",
+    "url": "/evolveu-react/static/media/002-game.4c2748a2.svg"
+  },
+  {
+    "revision": "0e5cbe3fbbc66f77087f12f3e8ca4ed2",
+    "url": "/evolveu-react/static/media/003-account.0e5cbe3f.svg"
+  },
+  {
+    "revision": "2bd9ed31e7cc2591a2e02f3f18235074",
+    "url": "/evolveu-react/static/media/004-city.2bd9ed31.svg"
+  },
+  {
+    "revision": "abdf1f925c10c945f29ab7d3622bb5be",
+    "url": "/evolveu-react/static/media/005-link.abdf1f92.svg"
+  },
+  {
+    "revision": "059ab19d73ba90221e28001b56381991",
+    "url": "/evolveu-react/static/media/006-stack.059ab19d.svg"
+  },
+  {
+    "revision": "08cee20e1141e22b07dcdc5f435a49ff",
+    "url": "/evolveu-react/static/media/logo.08cee20e.svg"
+  }
+]);
